4d270f166f5b482e719f7a216db1b8f184b76806
Gabbyton <gponon2000@gmail.com>
2025-10-21 11:43:17 -0500

    updated scripts to take terminal arguments

--- tests/scripts/generate_json_read_output.py
+++ tests/scripts/generate_json_read_output.py
@@ -1,19 +1,22 @@
 import json
+import sys
 
 from cemento.draw_io.read_diagram import read_drawio
 
-INPUT_PATH = "/Users/gponon/dev/CEMENTO/tests/test_files/read-diagram-03.drawio"
-OUTPUT_PATH = "/Users/gponon/dev/CEMENTO/tests/test_refs/read-diagram-03.json"
-
 if __name__ == "__main__":
-    elements, all_terms, triples, output_containers = read_drawio(INPUT_PATH)
+    if len(sys.argv) < 3:
+        raise ValueError("you must input an input and output path")
+    input_path = sys.argv[1]
+    output_path = sys.argv[2]
+
+    elements, all_terms, triples, output_containers = read_drawio(input_path)
     term_dict = {term_id: elements[term_id].get("value", None) for term_id in all_terms}
     triples = [list(map(lambda term: term_dict[term], triple)) for triple in triples]
     output_containers = {
         term_dict[key]: list(map(lambda term: term_dict[term], values))
         for key, values in output_containers
     }
-    with open(OUTPUT_PATH, "w") as f:
+    with open(output_path, "w") as f:
         output_dict = {
             "term_dict": term_dict,
             "triples": triples,
